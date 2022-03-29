4f34a9e251946ac7892f728dfed5d43c0b16a51b
Shoaib Ahamed Shafi <82079963+shoaib-ahamed@users.noreply.github.com>
2022-03-29 19:06:24 +0600

    boiler-plate v1.0

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -29,9 +29,9 @@ const Profile = ({user}) => {
         </div>
         <div>
             <form onSubmit={handleSubmit} className="grid justify-between align-middle mt-2">
-                <div className="form-group flex justify-between align-middle w-96">
+                <div className="form-group flex justify-between items-center w-max">
                     <p>Set a username : </p>  
-                    <input type="text" className="form-control border-2 w-15 rounded-md p-1" name="username" value={userName} placeholder="ex. themonstarcar" onChange={handleChangeInput}></input>
+                    <input type="text" className="form-control border-2 w-15 rounded-md p-2 m-3" name="username" value={userName} placeholder="ex. themonstarcar" onChange={handleChangeInput}></input>
                 </div>
                 <button className="bg-black text-white text-sm rounded font-light h-10 w-25 mt-2" type="submit" disabled={isUserUpdating}>Change Username</button>
             </form>
