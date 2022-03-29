b4da240fb7f2bbcbde89ebf78a0bcccf8acb5756
Shoaib Ahamed Shafi <82079963+shoaib-ahamed@users.noreply.github.com>
2022-03-29 18:45:39 +0600

    boiler-plate v1.0

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -59,15 +59,33 @@ const Navbar = () => {
         <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
       )}
       {toggleMenu && (
-        <ul
-          className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-          flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
-        >
-          <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-          {["Market", "Exchange", "Tutorials", "Wallets"].map(
-            (item, index) => <NavItem key={item + index} title={item} classprops="my-2 text-lg" />,
-          )}
-        </ul>
+         <div className="display-flex justify-between">
+             
+          <ul
+            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+          >
+            <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
+            {["Market", "Exchange", "Tutorials", "Wallets"].map(
+              (item, index) => <NavItem key={item + index} title={item} classprops="my-2 text-lg" />,
+            )}
+            {(isAuthenticated) ?
+      <>
+      <button onClick={disconnectWallet}>
+       <li className="bg-[#2952e3] mt-5 py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+       Logout
+     </li>
+     </button> 
+    </>
+       : 
+       <button onClick={connectWallet}>
+       <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+       Login
+     </li>
+     </button> 
+      }
+          </ul>
+          </div>
       )}
     </div>
   </nav>
