ebb90b6bb289383ab9836a29195f4e5e88ab93e5
Shoaib Ahamed Shafi <82079963+shoaib-ahamed@users.noreply.github.com>
2022-04-14 05:03:24 +0600

    transaction_complete

--- components/SendEth.jsx
+++ components/SendEth.jsx
@@ -2,26 +2,30 @@ import Moralis from 'moralis'
 import React, { useContext, useState } from 'react'
 import { useWeb3Transfer } from 'react-moralis'
 import { DataContext } from '../store/GlobalState'
+import Loader from './Loader'
 
 const inputBox = `className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism`
 
+
+const toast = (
+    <div className="text-5xl text-white bg-yellow-500 top-4 right-6">
+        i am transaction
+    </div>
+)
+
+
+
+
 const SendEth = () => {
 
-    const initialState = { reciever: '', amount: 0.2 }
+    const initialState = { receiver: '', amount: 0.2 }
     const [info, setInfo] = useState(initialState)
-    const { reciever, amount} = info
+    const { receiver, amount} = info
     const [loading, setLoading] = useState(false)
 
     const [state , dispatch] = useContext(DataContext)
 
-
-
-
-    const {fetch , isFetching} = useWeb3Transfer({
-        amount: Moralis.Units.ETH(amount),
-        reciever: reciever,
-        type: "native"
-    })
+  
 
 
     const handleChangeInput = e => {
@@ -29,17 +33,25 @@ const SendEth = () => {
         setInfo({...info, [name]:value})
     }
 
+    const {fetch , isFetching} = useWeb3Transfer({
+        amount: Moralis.Units.ETH("0.004"),
+        receiver: receiver,
+        type: "native"
+    })
+
     const handleSubmit = async e => {
         e.preventDefault()
         console.log('is it working')
         await Moralis.enableWeb3()
         console.log('is it working after enableWeb3')
+        console.log('receiver : ' , receiver)
         fetch({
             onSuccess: () => {
+                setLoading(false)
                 console.log('success')
-                dispatch({type: 'NOTIFY' , payload: {success: "Eth successfully sent"}})
             },
             onError: (error) => {
+                console.log(error)
                 dispatch({type: 'NOTIFY' , payload: {error: error}})
             }
         }) 
@@ -49,9 +61,9 @@ const SendEth = () => {
     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <form className="mx-auto my-4" onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <label className="text-white" htmlFor="name">Reciever</label>
+                    <label className="text-white" htmlFor="name">Receiver</label>
                     <input type="text" className={inputBox}
-                    name="reciever" value={reciever} onChange={handleChangeInput} />
+                    name="receiver" value={receiver} onChange={handleChangeInput} />
                 </div>
                 
                 <div className="form-group">
@@ -61,18 +73,17 @@ const SendEth = () => {
 
                 <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-                {/* {(loading)
+                {(loading)
               ? <Loader />
-              : ( */}
+              : (
                 <button
                   type="submit"
                   disabled={isFetching}
-                //   onClick={e => setLoading(true)}
                   className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
                 >
                   Send now
                 </button>
-              {/* )} */}
+              )} 
           
             </form>
 
